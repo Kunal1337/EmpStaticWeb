3ba746c17130674d4bfcf4e6c7439d24eaad9f51
unknown <kunal31905@gmail.com>
2025-10-26 19:52:25 -0500

    EmpStaticWeb: deploy employeeFormCRUD frontend (Vite) with SWA workflow

---
+++ src/App.simple.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ margin: '20px' }}>
+      <h1>Simple Test</h1>
+      <p>If you can see this, React is working!</p>
+    </div>
+  );
+}
+
+export default App;
